d5872f15042df9d624bcf8709c5a67d4541a4be8
notfresh_macbookpro <1195056983@qq.com>
2019-05-10 22:09:20 +0800

    day510 list-usage

--- day510_list_usage.py
+++ day510_list_usage.py
@@ -24,3 +24,12 @@ print(list1)
 print(list1[:-1])
 # list截取到倒数第二个，不包含第二个
 print(list1[:-2])
+
+
+#
+print('*'*100)
+list1 = [1,2]
+list2 = [[2,3], [4,5]]
+# 如何给list2的每个元素都加上list1的元素
+list2 = [list1 + item for item in list2]
+print(list2)
