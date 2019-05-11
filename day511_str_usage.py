509bea706afdf248c1606c693609f22601285cc3
notfresh_macbookpro <1195056983@qq.com>
2019-05-11 14:55:09 +0800

    day511 str-usage

--- day511_str_usage.py
+++ day511_str_usage.py
@@ -9,3 +9,15 @@ print(a[:a.index('2')]+a[a.index('2')+1:])
 
 print(list(a).remove('1'))
 # 经过尝试失败， 因为remove方法执行正常是不会返回原值的。
+
+
+# 如何依次切割12345
+print("*"*100)
+a = '12345'
+print(a[0:0]+a[1:])
+print(a[0:1]+a[2:])
+print(a[0:2]+a[3:])
+print(a[0:3]+a[4:])
+print(a[0:4]+a[5:])  # 这里很厉害， python可以自己动检测出越界下标，从而忽略掉。
+# 比如下面这个
+print(a[5:])  # 返回 None
